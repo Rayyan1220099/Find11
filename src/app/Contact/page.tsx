74b4f2b4e89c788523c2babb0bfc99d9219f743a
Rayyan1220099 <rayyan50467@gmail.com>
2025-06-18 18:34:01 +0500

    page.tsx

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -6,95 +6,129 @@ export default function Navbar() {
 
   return (
   <div className="bg-gradient-to-r from-[#F8F8F8] to-[#858484] ">
-    <section className="text-gray-600 body-font relative ">
-  <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
-    <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg border-6 border-black overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-      <iframe
-        width="100%"
-        height="100%"
-        className="absolute inset-0"
-        frameBorder={0}
-        title="map"
-        marginHeight={0}
-        marginWidth={0}
-        scrolling="no"
-        src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
-        style={{ filter: "grayscale(1) contrast(1.2) opacity(0.4)" }}
-      />
-     
-      <div className="bg-white relative flex flex-wrap py-6  border-4 border-black rounded shadow-md">
-        <div className="lg:w-1/2 px-6">
-          <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
-            ADDRESS
-          </h2>
-          <p className="mt-1">
-           JI. Raya Pakisaji, No.78 Bendo, Pakisajin Malang
-          </p>
-        </div>
-        <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
-          <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
-            EMAIL
-          </h2>
-          <a className="text-indigo-500 leading-relaxed">example@email.com</a>
-          <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">
-            PHONE
-          </h2>
-          <p className="leading-relaxed">123-456-7890</p>
-        </div>
-      </div>
-    </div>
-    <div className="lg:w-1/3 md:w-1/2 from-[#F8F8F8] to-[#858484] flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
-     <div className="border-4 border-black bg-white mb-3">
-      <h2 className="text-gray-900 text-4xl mb-1 font-medium title-font">
-       Contact us
-      </h2>
-      <p className="leading-relaxed mb-5 text-black text-lg">
-       Contact us to join, book, or build — we’re always ready for the next play.
+<section className="text-gray-600 body-font relative">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="flex flex-col text-center w-full mb-12">
+      <h1 className=" text-4xl font-bold title-font mb-4 text-gray-900">
+        Contact Us
+      </h1>
+      <p className="lg:w-2/3 mx-auto leading-relaxed text-xl">
+    Ready to chat football, bookings, or anything else? We’re always here to listen
       </p>
+    </div>
+    <div className="lg:w-1/2 md:w-2/3 mx-auto">
+      <div className="flex flex-wrap -m-2">
+        <div className="p-2 w-1/2">
+          <div className="relative">
+            <label htmlFor="name" className="leading-7 text-xl text-gray-600">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+        </div>
+        <div className="p-2 w-1/2">
+          <div className="relative">
+            <label htmlFor="email" className="leading-7 text-xl text-gray-600">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+        </div>
+        <div className="p-2 w-full">
+          <div className="relative">
+            <label
+              htmlFor="message"
+              className="leading-7 text-xl text-gray-600"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              className="w-full bg-gray-100 text-xl bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              defaultValue={""}
+            />
+          </div>
+        </div>
+        <div className="p-2 w-full">
+          <button className="flex mx-auto bg-[#333333] text-[#FFFFFF] border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            Button
+          </button>
+        </div>
+        <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+          <a className="text-indigo-500">example@email.com</a>
+          <p className="leading-normal my-5">
+            49 Smith St.
+            <br />
+            Saint Cloud, MN 56301
+          </p>
+          <span className="inline-flex">
+            <a className="text-gray-500">
+              <svg
+                fill="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                className="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
+              </svg>
+            </a>
+            <a className="ml-4 text-gray-500">
+              <svg
+                fill="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                className="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
+              </svg>
+            </a>
+            <a className="ml-4 text-gray-500">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                className="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <rect width={20} height={20} x={2} y={2} rx={5} ry={5} />
+                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
+              </svg>
+            </a>
+            <a className="ml-4 text-gray-500">
+              <svg
+                fill="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                className="w-5 h-5"
+                viewBox="0 0 24 24"
+              >
+                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
+              </svg>
+            </a>
+          </span>
+        </div>
       </div>
-      <div className="relative mb-4 ">
-        <label htmlFor="name" className="leading-7 text-lg text-black">
-          Name
-        </label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          className="w-full bg-white rounded border-4  border-black focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-        />
-      </div>
-      <div className="relative mb-4">
-        <label htmlFor="email" className="leading-7 text-lg text-black">
-          Email
-        </label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          className="w-full bg-white rounded border-4 border-black  focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-        />
-      </div>
-      <div className="relative mb-4">
-        <label htmlFor="message" className="leading-7 text-lg text-black">
-          Message
-        </label>
-        <textarea
-          id="message"
-          name="message"
-          className="w-full bg-white rounded  border-4 border-black focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-          defaultValue={""}
-        />
-      </div>
-       <Link
-            href="/Contact"
-            className="bg-[#333333] text-[#FFFFFF] px-4 py-2 rounded-md font-semibold hover:opacity-90 transition"
-          >
-          Get Started
-          </Link>
     </div>
   </div>
 </section>
-
   </div>
 
   )}
